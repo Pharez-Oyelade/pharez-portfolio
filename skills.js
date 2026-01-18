7eb301ee109f47ab965b6ccceb419ad2752f7614
Pharez <pharezoyelade@gmail.com>
2026-01-18 19:51:45 +0100

    skills section added

---
+++ skills.js
@@ -0,0 +1,29 @@
+const skillsData = [
+  {
+    section: "Core",
+    skills: [
+      { name: "HTML", icon: "html-icon-path" },
+      { name: "CSS", icon: "css-icon-path" },
+      { name: "JavaScript", icon: "js-icon-path" },
+      { name: "React", icon: "react-icon-path" },
+    ],
+  },
+  {
+    section: "Tools",
+    skills: [
+      { name: "Git", icon: "git-icon-path" },
+      { name: "Vite", icon: "vite-icon-path" },
+      { name: "Tailwind CSS", icon: "tailwind-icon-path" },
+      { name: "Figma", icon: "figma-icon-path" },
+    ],
+  },
+  {
+    section: "Backend",
+    skills: [
+      { name: "Node.js", icon: "node-icon-path" },
+      { name: "Express", icon: "express-icon-path" },
+      { name: "MongoDB", icon: "mongo-icon-path" },
+      { name: "Firebase", icon: "firebase-icon-path" },
+    ],
+  },
+];
